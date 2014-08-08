fadc53332af342134eff905f0fb25b1240717a04
Arne Ruhnau <arne.ruhnau@target-sg.com>
2014-08-08 12:07:46 +0200

    Added board support for the pconxs, U100, and F500

---
+++ include/configs/mx6dltrizeps7pconxs.h
@@ -0,0 +1,9 @@
+#ifndef __MX6DLTRIZEPS7PCONXS_CONFIG_H
+#define  __MX6DLTRIZEPS7PCONXS_CONFIG_H
+
+#define CONFIG_DEFAULT_FDT_FILE		"imx6dl-trizeps7-pconxs.dtb"
+#define CONFIG_BOARD_NAME "pConXS"
+
+#include "mx6dltrizeps7.h"
+
+#endif
---
+++ include/configs/mx6dltrizeps7u100.h
@@ -0,0 +1,9 @@
+#ifndef __MX6DLTRIZEPS7U100_CONFIG_H
+#define  __MX6DLTRIZEPS7U100_CONFIG_H
+
+#define CONFIG_DEFAULT_FDT_FILE		"imx6dl-trizeps7-u100.dtb"
+#define CONFIG_BOARD_NAME "U100"
+
+#include "mx6dltrizeps7.h"
+
+#endif
